07735d1c4d4e1e56cec7ed60fb91790ea2f631de
pezzolesi <lorenzo.pezzolesi@studenti.isissgobetti.it>
2024-10-28 09:03:12 +0100

    es 7

---
+++ pezzolesi_es_7.py
@@ -0,0 +1,25 @@
+class Materialebibioteca:
+    def __init__(self,titolo: str, anno_pubblicazzione: int, disponibile: True):
+        self.titolo = titolo
+        self.anno_pubblicazzione = anno_pubblicazzione
+        self.disponibile = disponibile
+
+    def prestito(disponibile):
+        disponibile = False
+
+    def restituzione(disponibile):
+        disponibile = True
+
+    def get_titolo(self):
+
+
+
+
+
+
+
+
+
+
+
+
